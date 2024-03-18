0d9be03d5bbd2c7b22e0da4e286c01779110d576
AgustinSegui <agussegui24@hotmail.es>
2024-03-18 18:14:49 -0300

    Mini Proyecto de carrito de compras terminado con LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState } from "react"
+import {useState, useEffect } from "react"
 
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
@@ -6,8 +6,17 @@ import { db } from "./data/db"
 
 function App() {
 
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
   
   function addToCart(item) {
 
